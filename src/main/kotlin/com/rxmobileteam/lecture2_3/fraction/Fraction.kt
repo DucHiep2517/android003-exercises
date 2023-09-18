f57732679922e61d0d729429388d93b9e7dc2ef8
love.giant2000@gmail.com <100131715+DucHiep2517@users.noreply.github.com>
2023-09-18 23:11:14 +0700

    example_2

---
+++ src/main/kotlin/com/rxmobileteam/lecture2_3/fraction/Fraction.kt
@@ -0,0 +1,209 @@
+package com.rxmobileteam.lecture2_3.fraction
+
+class Fraction private constructor(
+    val numerator: Int,
+    val denominator: Int,
+) : Comparable<Fraction> {
+    // TODO: Implement the decimal value of the fraction
+    val decimal: Double = (numerator/denominator).toDouble()
+
+    init {
+        // TODO: Check validity of numerator and denominator (throw an exception if invalid)
+        if(denominator == 0)
+            throw ArithmeticException("denominator = 0 is not compatible")
+    }
+
+    //region unary operators
+    // TODO: "+fraction" operator
+    operator fun unaryPlus(): Fraction {
+        return +of(numerator, denominator)
+    }
+
+    // TODO: "-fraction" operator
+    operator fun unaryMinus(): Fraction {
+        return -of(numerator, denominator)
+    }
+    //endregion
+
+    //region plus operators
+    // TODO: "fraction+fraction" operator
+    operator fun plus(other: Fraction): Fraction {
+        // Implement addition of fractions
+        val commonDenominator = this.denominator * other.denominator
+        val newNumerator = (this.numerator * other.denominator) + (other.numerator * this.denominator)
+        return of(newNumerator, commonDenominator)
+    }
+
+    // TODO: "fraction+number" operator
+    operator fun plus(other: Number): Fraction {
+        val commonDenominator = this.denominator
+        val newNumerator = (this.numerator) + (other.toInt() * this.denominator)
+        return of(newNumerator, commonDenominator)
+    }
+    //endregion
+
+    //region times operators
+    // TODO: "fraction*fraction" operator
+    operator fun times(other: Fraction): Fraction {
+        val commonDenominator = this.denominator * other.denominator
+        val newNumerator = this.numerator * other.numerator
+        return of(newNumerator, commonDenominator)
+    }
+
+    // TODO: "fraction*number" operator
+    operator fun times(number: Number): Fraction {
+        val newNumerator = this.numerator * number.toInt()
+        return of(newNumerator, this.denominator)
+    }
+    //endregion
+
+    // TODO: Compare two fractions
+    override fun compareTo(other: Fraction): Int {
+        val thisNumerator = this.numerator * other.denominator
+        val otherNumerator = other.numerator * this.denominator
+
+        return thisNumerator.compareTo(otherNumerator)
+    }
+
+    //region toString, hashCode, equals, copy
+    // TODO: Format the fraction as a string (e.g. "1/2")
+    override fun toString(): String {
+        return "${this.numerator}/${this.denominator}"
+    }
+
+    // TODO: Implement hashCode
+    override fun hashCode(): Int = TODO()
+
+    // TODO: Implement equals
+    override fun equals(other: Any?): Boolean = TODO()
+
+    // TODO: Implement copy
+    fun copy(
+        numerator: Int = this.numerator,
+        denominator: Int = this.denominator
+    ): Fraction {
+        return of(numerator, denominator)
+    }
+    //endregion
+
+    companion object {
+        @JvmStatic
+        fun ofDouble(decimal: Double): Fraction {
+            // TODO: Returns a fraction from a decimal number
+            return Fraction(0, 0) // Change this
+        }
+
+        @JvmStatic
+        fun ofInt(number: Int): Fraction {
+            // TODO: Returns a fraction from an integer number
+            return Fraction(number, 1) // Change this
+        }
+
+        @JvmStatic
+        fun of(numerator: Int, denominator: Int): Fraction {
+            // TODO: Check validity of numerator and denominator
+            // TODO: Simplify fraction using the greatest common divisor
+            // TODO: Finally, return the fraction with the correct values
+            return Fraction(numerator, denominator) // Change this
+        }
+    }
+}
+
+// TODO: return a Fraction representing "this/denominator"
+infix fun Int.over(denominator: Int): Fraction = Fraction.of(this, denominator)
+
+//region get extensions
+// TODO: get the numerator, eg. "val (numerator) = Fraction.of(1, 2)"
+operator fun Fraction.component1(): Int = TODO()
+
+// TODO: get the denominator, eg. "val (_, denominator) = Fraction.of(1, 2)"
+operator fun Fraction.component2(): Int = TODO()
+
+// TODO: get the decimal, index must be 0 or 1
+// TODO: eg. "val numerator = Fraction.of(1, 2)[0]"
+// TODO: eg. "val denominator = Fraction.of(1, 2)[1]"
+// TODO: eg. "val denominator = Fraction.of(1, 2)[2]" should throw an exception
+operator fun Fraction.get(index: Int): Int = TODO()
+//endregion
+
+//region to number extensions
+// TODO: round to the nearest integer
+fun Fraction.toInt(): Int = TODO()
+
+// TODO: round to the nearest long
+fun Fraction.toLong(): Long = TODO()
+
+// TODO: return the decimal value as a float
+fun Fraction.toFloat(): Float = TODO()
+
+// TODO: return the decimal value as a double
+fun Fraction.toDouble(): Double = TODO()
+//endregion
+
+fun main() {
+    // Creation
+    println("1/2: ${Fraction.of(1, 2)}")
+    println("2/3: ${Fraction.of(2, 3)}")
+    println("1.6: ${Fraction.ofDouble(1.6)}")
+    println("8: ${Fraction.ofInt(8)}")
+    println("2/4: ${2 over 4}")
+
+    // Unary operators
+    println("+2/4: ${+Fraction.of(2, 4)}")
+    println("-2/6: ${-Fraction.of(2, 6)}")
+
+    // Plus operators
+    println("1/2 + 2/3: ${Fraction.of(1, 2) + Fraction.of(2, 3)}")
+    println("1/2 + 1: ${Fraction.of(1, 2) + 1}")
+
+    // Times operators
+    println("1/2 * 2/3: ${Fraction.of(1, 2) * Fraction.of(2, 3)}")
+    println("1/2 * 2: ${Fraction.of(1, 2) * 2}")
+
+    // compareTo
+    println("3/2 > 2/2: ${Fraction.of(3, 2) > Fraction.of(2, 2)}")
+    println("1/2 <= 2/4: ${Fraction.of(1, 2) <= Fraction.of(2, 4)}")
+    println("4/6 >= 2/3: ${Fraction.of(4, 6) >= Fraction.of(2, 3)}")
+
+    // hashCode
+    println("hashCode 1/2 == 2/4: ${Fraction.of(1, 2).hashCode() == Fraction.of(2, 4).hashCode()}")
+    println("hashCode 1/2 == 1/2: ${Fraction.of(1, 2).hashCode() == Fraction.of(1, 2).hashCode()}")
+    println("hashCode 1/3 == 3/5: ${Fraction.of(1, 3).hashCode() == Fraction.of(3, 5).hashCode()}")
+
+    // equals
+    println("1/2 == 2/4: ${Fraction.of(1, 2) == Fraction.of(2, 4)}")
+    println("1/2 == 1/2: ${Fraction.of(1, 2) == Fraction.of(1, 2)}")
+    println("1/3 == 3/5: ${Fraction.of(1, 3) == Fraction.of(3, 5)}")
+
+    // Copy
+    println("Copy 1/2: ${Fraction.of(1, 2).copy()}")
+    println("Copy 1/2 with numerator 2: ${Fraction.of(1, 2).copy(numerator = 2)}")
+    println("Copy 1/2 with denominator 3: ${Fraction.of(1, 2).copy(denominator = 3)}")
+    println("Copy 1/2 with numerator 2 and denominator 3: ${Fraction.of(1, 2).copy(numerator = 2, denominator = 3)}")
+
+    // Component1, Component2 operators
+    val (numerator, denominator) = Fraction.of(1, 2)
+    println("Component1, Component2 of 1/2: $numerator, $denominator")
+    val (numerator2, _) = Fraction.of(10, 30)
+    println("Component1 of 10/30: $numerator2")
+    val (_, denominator2) = Fraction.of(10, 79)
+    println("Component2 of 10/79: $denominator2")
+
+    // Get operator
+    println("Get 0 of 1/2: ${Fraction.of(1, 2)[0]}")
+    println("Get 1 of 1/2: ${Fraction.of(1, 2)[1]}")
+    println("Get 2 of 1/2: ${runCatching { Fraction.of(1, 2)[2] }}") // Should print "Failure(...)"
+
+    // toInt, toLong, toFloat, toDouble
+    println("toInt 1/2: ${Fraction.of(1, 2).toInt()}")
+    println("toLong 1/2: ${Fraction.of(1, 2).toLong()}")
+    println("toFloat 1/2: ${Fraction.of(1, 2).toFloat()}")
+    println("toDouble 1/2: ${Fraction.of(1, 2).toDouble()}")
+
+    // Range
+    // Because we implemented Comparable<Fraction>, we can use Fraction in ranges
+    val range = Fraction.of(1, 2)..Fraction.of(2, 3)
+    println("1/2 in range 1/2..2/3: ${Fraction.of(1, 2) in range}") // "in" operator is contains
+    println("2/3 in range 1/2..2/3: ${Fraction.of(2, 3) in range}")
+    println("7/12 in range 1/2..2/3: ${Fraction.of(7, 12) in range}")
+}
---
+++ src/main/kotlin/com/rxmobileteam/lecture2_3/products/product.kt
@@ -0,0 +1,249 @@
+package com.rxmobileteam.lecture2_3.products
+
+import java.util.*
+
+enum class ProductCategory {
+    LAPTOP,
+    PHONE,
+    HEADPHONES,
+    SMART_WATCH,
+    CAMERA,
+}
+
+data class Product(
+    val id: String,
+    val name: String,
+    val price: Double,
+    val category: ProductCategory,
+    val favoriteCount: Int,
+)
+
+data class Order(
+    val id: String,
+    val products: List<Product>,
+    val isDelivered: Boolean,
+)
+
+// TODO: Return a list of Product, sorted in the ascending by price. if prices are equal, sorted by favoriteCount descending
+fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> {
+    this.sortedWith(compareBy<Product> { it.price }
+        .thenByDescending { it.favoriteCount })
+
+    return this
+}
+
+// TODO: Return a set of Products in the orders (The order doesn't matter).
+fun List<Order>.getProductsSet(): Set<Product> {
+//    var orders = mutableSetOf<Product>()
+//    for (e in this) {
+//        for(v in e.products){
+//            orders.add(v)
+//        }
+//    }
+
+    return this.getProductsList().toSet()
+}
+
+// TODO: Return a list of Products in the orders, duplicates are allowed.
+fun List<Order>.getProductsList(): List<Product> {
+    val products = mutableListOf<Product>()
+    for (order in this) {
+        for(product in order.products){
+            products.add(product)
+        }
+    }
+    return products
+}
+
+// TODO: Return a list of delivered orders
+fun List<Order>.getDeliveredOrders(): List<Order> {
+    val orders = mutableListOf<Order>()
+    for (order in this) {
+        if(order.isDelivered) {
+            orders.add(order)
+        }
+    }
+
+    return orders
+}
+
+// TODO: Return a list of products in the delivered orders
+fun List<Order>.getDeliveredProductsList(): List<Product> {
+    return this.getDeliveredOrders().getProductsList()
+}
+
+// TODO: Partition the orders into two lists: "delivered" and "not delivered"
+fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> {
+    val (deliveredOrders, notDeliveredOrders) = this.partition { it.isDelivered }
+    return Pair(deliveredOrders, notDeliveredOrders)
+}
+
+// TODO: Return a map of product to count of this product in the orders
+// eg. [Product1 -> 2, Product2 -> 1, Product3 -> 3]
+fun List<Order>.countOfEachProduct(): Map<Product, Int> {
+    val productCountMap = mutableMapOf<Product, Int>()
+
+    for (order in this) {
+        for (product in order.products) {
+            val count = product.favoriteCount
+            productCountMap[product] = count
+        }
+    }
+    return productCountMap
+}
+
+// TODO: Return the sum of product prices in the order
+fun Order.sumProductPrice(): Double = this.products.sumOf { it.price }
+
+// TODO: Return the product with the maximum price in the order
+fun Order.getMaxPriceProduct(): Product? = this.products.maxByOrNull { it.price }
+
+// TODO: Return the product with the min price in the order
+fun Order.getMinPriceProduct(): Product? = this.products.minByOrNull { it.price }
+
+val product = Product(
+    id = UUID.randomUUID().toString(),
+    name = "Sandy Short Special Edition",
+    price = 2.3,
+    category = ProductCategory.LAPTOP,
+    favoriteCount = 1,
+)
+
+val productList = listOf(
+    product,
+    Product(
+        id = UUID.randomUUID().toString(),
+        name = "Stacie Riddle",
+        price = 6.7,
+        category = ProductCategory.PHONE,
+        favoriteCount = 2,
+    ),
+    Product(
+        id = UUID.randomUUID().toString(),
+        name = "Stacie Riddle",
+        price = 6.7,
+        category = ProductCategory.LAPTOP,
+        favoriteCount = 3,
+    ),
+    Product(
+        id = UUID.randomUUID().toString(),
+        name = "Stacie Riddle",
+        price = 6.7,
+        category = ProductCategory.SMART_WATCH,
+        favoriteCount = 4,
+    ),
+    Product(
+        id = UUID.randomUUID().toString(),
+        name = "Stacie Riddle",
+        price = 1.0,
+        category = ProductCategory.HEADPHONES,
+        favoriteCount = 5,
+    ),
+    Product(
+        id = UUID.randomUUID().toString(),
+        name = "Stacie Riddle",
+        price = 10.0,
+        category = ProductCategory.CAMERA,
+        favoriteCount = 0,
+    ),
+)
+
+val orderList = listOf(
+    Order(
+        id = UUID.randomUUID().toString(),
+        products = listOf(
+            product,
+            Product(
+                id = UUID.randomUUID().toString(),
+                name = "Stacie Riddle",
+                price = 6.7,
+                category = ProductCategory.PHONE,
+                favoriteCount = 2,
+            ),
+        ),
+        isDelivered = true,
+    ),
+    Order(
+        id = UUID.randomUUID().toString(),
+        products = listOf(
+            product,
+            Product(
+                id = UUID.randomUUID().toString(),
+                name = "Stacie Riddle",
+                price = 100.0,
+                category = ProductCategory.SMART_WATCH,
+                favoriteCount = 3,
+            ),
+        ),
+        isDelivered = false,
+    ),
+    Order(
+        id = UUID.randomUUID().toString(),
+        products = listOf(
+            product,
+            Product(
+                id = UUID.randomUUID().toString(),
+                name = "Stacie Riddle",
+                price = 6.7,
+                category = ProductCategory.PHONE,
+                favoriteCount = 2,
+            ),
+            Product(
+                id = UUID.randomUUID().toString(),
+                name = "Efrain Hawkins",
+                price = 100.0,
+                category = ProductCategory.CAMERA,
+                favoriteCount = 5235,
+            ),
+        ),
+        isDelivered = true,
+    ),
+)
+
+fun main() {
+    //region sortedByPriceAscendingThenByFavoriteCountDescending
+    println("sortedByPriceAscendingThenByFavoriteCountDescending")
+    println(productList.sortedByPriceAscendingThenByFavoriteCountDescending())
+    //endregion
+
+    //region getProductsSet
+    println("getProductsSet")
+    println(orderList.getProductsSet())
+    //endregion
+
+    //region getProductsList
+    println("getProductsList")
+    println(orderList.getProductsList())
+    //endregion
+
+    //region getDeliveredOrders
+    println("getDeliveredOrders")
+    println(orderList.getDeliveredOrders())
+    //endregion getDeliveredProductsList
+
+    //region getDeliveredProductsList
+    println("getDeliveredProductsList")
+    println(orderList.getDeliveredProductsList())
+    //endregion
+
+    //region partitionDeliveredAndNotDelivered
+    println("partitionDeliveredAndNotDelivered")
+    println(orderList.partitionDeliveredAndNotDelivered())
+    //endregion
+
+    //region countOfEachProduct
+    println("countOfEachProduct")
+    println(orderList.countOfEachProduct())
+    //endregion
+
+    //region sumProductPrice
+    println("sumProductPrice")
+    println(orderList[0].sumProductPrice())
+    //endregion
+
+    //region getMaxPriceProduct, getMinPriceProduct
+    println("getMaxPriceProduct, getMinPriceProduct")
+    println(orderList[0].getMaxPriceProduct())
+    println(orderList[0].getMinPriceProduct())
+    //endregion
+}
