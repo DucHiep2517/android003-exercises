a253b113e7487239eaaa17acc8072e7a2293796a
hiepnguyen <dev3t>
2023-09-25 09:14:38 +0700

    example_audio

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -39,7 +39,6 @@ class FirstFragment : Fragment() {
     ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
